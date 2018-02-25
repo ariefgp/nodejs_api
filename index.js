4287c4be47a1a0554632eba3521ae142e6751590
ariefgp <arief.gusti@gmail.com>
2018-02-25 15:15:18 +0700

    learn node js - install babel rc and creat simple get api

---
+++ index.js
@@ -0,0 +1,32 @@
+// ============== Contoh 1 ===============================
+
+// import express from "express";
+
+// const PORT = 3000;
+// const app = express();
+
+// app.get("/", (req, res) => res.json({status: "NTask API"}));
+
+// app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
+
+// ============== Contoh 2 ===============================
+
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.set("json spaces", 4);
+
+app.get("/", (req, res) => res.json({status: "NTask API"}));
+
+app.get("/tasks", (req, res) => {
+	res.json({
+		tasks: [
+			{title: "Buy some shoes"},
+			{title: "Fix notebook"}
+		]
+	});
+});
+
+app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
